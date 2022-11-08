759d68cc413a0fec90d73344c960e661fb922921
Samarth Kulshrestha <samarthkulshrestha@protonmail.com>
2022-11-08 22:05:20 +0530

    making the prompt cross-platform.

--- src/prompt.c
+++ src/prompt.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+
+#include <string.h>
+
+static char buffer[2048];
+
+// fake readline func
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy
+}
+
+// fake add_history func
+void add_history(char* unused) {}
+
+#else
+
 #include <editline/readline.h>
+#include <histedit.h>
+
+#endif
 
 
 int main(int argc, char** argv) {
@@ -11,6 +35,7 @@ int main(int argc, char** argv) {
     while (1) {
         char* input = readline("eclisp> ");
         add_history(input);
+
         printf("no! you're a %s\n", input);
         free(input);
     }
