eb6b06b7ad87d3d22a61e76d84ba0edfb00f52f6
Samarth Kulshrestha <samarthkulshrestha@protonmail.com>
2022-11-09 00:11:12 +0530

    implemented evaluation of the parsed language.

--- src/eclisp.c
+++ src/eclisp.c
@@ -29,6 +29,36 @@ void add_history(char* unused) {}
 #endif
 
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    // return directly if tagged as a number
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // the operator is always the second child
+    char* op = t->children[1]->contents;
+
+    // third child is x
+    long x = eval(t->children[2]);
+
+    // iterate over remaining children
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
     // parsers
     mpc_parser_t* Number = mpc_new("number");
@@ -56,8 +86,8 @@ int main(int argc, char** argv) {
         // attempt to parse user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Eclisp, &r)){
-            // on success print AST
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             // otherwise print error
